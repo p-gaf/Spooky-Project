3185d2e4413e1a21ae4a70f27b5a6f37dc71862d
Brendan Abernathy <brendan.abernathy@colorado.edu>
2018-10-19 11:52:56 -0600

    Update spooky.py
    Started work on setup function and gui.

--- spooky.py
+++ spooky.py
@@ -1,6 +1,10 @@
+import gui
 
+window = None
 
-
+def setup():
+  global window
+  window = gui.Display("Minecard", 640,480)
 
 #playsound function to be activated when a function is clicked on
 def play(file):
