416c25eac1364e9a866be899dbc95986031087db
Brendan Abernathy <brendan.abernathy@colorado.edu>
2018-10-19 12:06:53 -0600

    Update spooky.py

--- spooky.py
+++ spooky.py
@@ -1,11 +1,22 @@
 import gui
+import timer
 
 
 window = None
+fadeRectangle = None
+frameTimer = None
 
 def setup():
-  global window
+  global window, frameTimer
   window = gui.Display("Minecard", 640,480)
+  
+  fadeRectangle = gui.Rectangle(0,0,640,480,gui.Color(0,0,0,255),true,0)
+  window.add(fadeRectangle)
+  
+  frameTimer = timer.Timer(1000/60, update, [], true)
+
+def update():
+  pass
 
 #make and play function to be activated when a function is clicked on
 def makeAndPlay(file):
