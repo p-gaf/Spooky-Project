e22369c68e4779252accd94077aa81e37771e1f2
PatrickG <ptrgaffney@gmail.com>
2018-10-25 18:21:54 -0600

    added frames for creeper and some other code

--- spooky.py
+++ spooky.py
@@ -30,9 +30,19 @@ def monsterClick(x,y):
   #Include a file extension
   makeAndPlay(file)
   #Add an animation function
+#create frames for creeper gif
+def createIconsForCreeper(suffix):
+  output=[]
+  for i in range(0,51):
+    filename="frame_0"+str(i)+"_delay-0.05s.gif"
+    icon=gui.Icon(filename,300)
+    output.append(icon)
+    
+    
+ 
+
 
 #Insert icons into gui window, 
-creeper=gui.Icon(,random.randInt(0,640),random.randInt(0,480))
 zombie=gui.Icon(,random.randInt(0,640),random.randInt(0,480))
 skeleton=gui.Icon(,random.randInt(0,640),random.randInt(0,480))
 
